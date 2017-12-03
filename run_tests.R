dbd37496d001c99c86238f26d3152b662bbf052a
paulgovan <pgovan1@aggienetwork.com>
2017-12-02 18:00:35 -0600

    Update travis

--- run_tests.R
+++ run_tests.R
@@ -1,5 +1,4 @@
-library(testthat)
-library(shinytest)
+install.packages("Cairo", INSTALL_opts = "--no-test-load")
 
 testthat::test_that("Application works", {
   shinytest::expect_pass(shinytest::testApp("inst/bn", "mytest", compareImages = FALSE))
